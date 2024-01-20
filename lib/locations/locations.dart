d821de351b56d9f25bbcffb6a74ba8ef02d2d8ec
001sara001 <malihatasnim530@gmail.com>
2024-01-20 15:25:45 +0600

    changes

---
+++ lib/locations/locations.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/screens/user.dart';
+import 'package:untitled1/screens/restaurant.dart';
+import 'package:untitled1/screens/applogo.dart';
+import 'package:untitled1/widgets/custom_scaffold.dart';
+
+class Locations extends StatefulWidget {
+  const Locations({Key? key}) : super(key: key);
+
+  @override
+  State<Locations> createState() => _LocationsState();
+}
+
+class _LocationsState extends State<Locations> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          applogo(),
+          SizedBox(height: 130), // Adjust the height as needed
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => User()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.transparent,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(color: Colors.white),
+              ),
+            ),
+            child: Container(
+              width: 200,
+              height: 80,
+              alignment: Alignment.center,
+              child: const Text(''
+                  'Mohammadpur', style: TextStyle(fontSize: 25, color: Colors.white)),
+            ),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Restaurant()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.transparent,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(color: Colors.white),
+              ),
+            ),
+            child: Container(
+              width: 200,
+              height: 80,
+              alignment: Alignment.center,
+              child: const Text('Mirpur', style: TextStyle(fontSize: 25, color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
